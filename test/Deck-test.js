c21e54be8e570f685f84997937bd43002b076344
Matt Rowan <matt.rowan121@gmail.com>
2022-11-29 17:48:53 -0500

    Remove remaining skips

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -27,7 +27,7 @@ describe('Deck', () => {
         expect(deck.allCards).to.include(card1, card2, card3);
     });
 
-    it.skip('should be able to count how many cards are in the deck', () => {
+    it('should be able to count how many cards are in the deck', () => {
         const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
         
         const deck = new Deck([card1, card2, card3]);
