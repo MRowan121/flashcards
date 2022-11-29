4fa2bb5ca601563414b5a8ae89254983fdbf0a83
Matt Rowan <matt.rowan121@gmail.com>
2022-11-29 08:43:46 -0500

    Remove all skips

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -12,7 +12,7 @@ describe('Turn', function() {
         expect(Turn).to.be.a('function');
     });
 
-    it.skip('should have a user guess and a Card', function() {
+    it('should have a user guess and a Card', function() {
         const card = new Card(1, 'What is my last name?', ['Matthew', 'Brandon', 'Rowan'], 'Rowan');
         const turn = new Turn('Matthew', card);
 
@@ -20,28 +20,28 @@ describe('Turn', function() {
         expect(turn.currentCard).to.equal(card);
     });
 
-    it.skip('should be able to return the user guess', function() {
+    it('should be able to return the user guess', function() {
         const card = new Card(1, 'What is my last name?', ['Matthew', 'Brandon', 'Rowan'], 'Rowan');
         const turn = new Turn('Matthew', card);
 
         expect(turn.returnGuess()).to.equal('Matthew');
     });
 
-    it.skip('should be able to return the current card details', function() {
+    it('should be able to return the current card details', function() {
         const card = new Card(1, 'What is my last name?', ['Matthew', 'Brandon', 'Rowan'], 'Rowan');
         const turn = new Turn('Matthew', card);
 
         expect(turn.returnCard()).to.equal(card);
     });
 
-    it.skip('should be able to evaluate the users guess', function() {
+    it('should be able to evaluate the users guess', function() {
         const card = new Card(1, 'What is my last name?', ['Matthew', 'Brandon', 'Rowan'], 'Rowan');
         const turn = new Turn('Matthew', card);
 
         expect(turn.evaluateGuess()).to.equal(false);
     });
 
-    it.skip('should be able to give feedback based on user guess', function() {
+    it('should be able to give feedback based on user guess', function() {
         const card = new Card(1, 'What is my last name?', ['Matthew', 'Brandon', 'Rowan'], 'Rowan');
         const turn = new Turn('Matthew', card);
 
