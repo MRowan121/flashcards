d6fd2025d84a3c598d76c6a586fc2ba077b8962e
Matt Rowan <matt.rowan121@gmail.com>
2022-11-29 17:49:09 -0500

    Add count cards method

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,11 @@
 class Deck {
+    constructor(newCards) {
+        this.allCards = newCards;
+    };
 
+    countCards() {
+        return this.allCards.length;
+    }
 };
 
 module.exports = Deck;
