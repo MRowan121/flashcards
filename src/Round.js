ae61b19530fbbbdbba5e1871813111f087203014
Matt Rowan <matt.rowan121@gmail.com>
2022-11-30 17:44:56 -0500

    Add take turn method

--- src/Round.js
+++ src/Round.js
@@ -1,13 +1,28 @@
+const Turn = require("./Turn");
+
 class Round {
     constructor(deck) {
         this.deck = deck;
         this.turns = 0;
         this.incorrectGuesses = [];
         this.correctGuesses = [];
+        this.percentCorrect = 0;
     };
 
     returnCurrentCard() {
         return this.deck.allCards[this.turns];
+    };
+
+    takeTurn(userGuess) {
+        let turn = new Turn(userGuess, this.returnCurrentCard())
+        this.turns ++;
+        if(turn.giveFeedback() === 'incorrect!') {
+            this.incorrectGuesses.push(turn.currentCard.id)
+            return turn.giveFeedback();
+        } else {
+            this.correctGuesses.push(turn.currentCard.id)
+            return turn.giveFeedback();
+        };
     };
 };
 
