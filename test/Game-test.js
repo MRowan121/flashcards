0b11aae827fcccd91be93af107fdb78f8da261d2
Matt Rowan <matt.rowan121@gmail.com>
2022-12-01 16:31:15 -0500

    Remove skips from tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -12,25 +12,25 @@ describe('Game', () => {
         game = new Game();
     });
 
-    it.skip('should be a function', () => {
+    it('should be a function', () => {
         expect(Game).to.be.a('function');
     });
 
-    it.skip('should be an instance of game', () => {
+    it('should be an instance of game', () => {
         expect(game).to.be.an.instanceOf(Game);
     });
 
-    it.skip('should start with undefined as current round', () => {
+    it('should start with undefined as current round', () => {
         expect(game.currentRound).to.equal(undefined);
     });
 
-    it.skip('should have a deck of cards', () => {
+    it('should have a deck of cards', () => {
         game.start();
-        expect(game.currentRound.deck.cards).to.be.an('array');
-        expect(game.currentRound.deck.cards).to.equal(30);
+        expect(game.currentRound.deck.allCards).to.be.an('array');
+        expect(game.currentRound.deck.allCards.length).to.equal(30);
     });
 
-    it.skip('should keep track of the current round', () => {
+    it('should keep track of the current round', () => {
         game.start();
         expect(game.currentRound).to.not.equal(undefined);
         expect(game.currentRound).to.be.an.instanceOf(Round);
